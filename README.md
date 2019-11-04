a1b2a2f4bff2b1eddd5d2e07a1b4f347f4076e3b
Kristaps Horns <k.horns@ideaspool.nl>
2019-11-04 19:57:31 +0100

    Init

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # parse-pdf-to-text-using-ruby
+
+Place your PDF files in folder /pdfs
+and run code from command line using
+
+ruby parse.rb
---
+++ parse.rb
@@ -0,0 +1,20 @@
+# gem install pdf-reader
+require 'pdf-reader'
+
+# files = Dir["pdfs/*.pdf"]
+files = Dir[ 'pdfs/*.pdf' ].select{ |f| File.file? f }.map{ |f| File.basename f }
+
+files.each do |file_name|
+
+	reader = PDF::Reader.new('pdfs/' + file_name)
+	puts "Converting : #{file_name}"
+	
+	reader.pages.each do |page|
+	  # puts page.text
+
+	  open('txts/' + file_name + '.txt', 'a') do |f|
+	  	f << page.text
+	  end
+	end
+
+end
