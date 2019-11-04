11a8f66503c037e502b81de4cdd8ec7dd9a4dfb1
Kristaps Horns <k.horns@ideaspool.nl>
2019-11-04 20:03:06 +0100

    Remove .DS_Store

--- parse.rb
+++ parse.rb
@@ -5,9 +5,9 @@ require 'pdf-reader'
 files = Dir[ 'pdfs/*.pdf' ].select{ |f| File.file? f }.map{ |f| File.basename f }
 
 files.each do |file_name|
+	puts "Converting : #{file_name}"
 
 	reader = PDF::Reader.new('pdfs/' + file_name)
-	puts "Converting : #{file_name}"
 	
 	reader.pages.each do |page|
 	  # puts page.text
